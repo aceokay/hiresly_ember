edbd182ec73b0562018c18f4dd0d33b217ddf929
Guest User <student@epicodus.com>
2015-10-05 15:00:57 -0700

    Add RSVP model retrieval for developers and employers on login

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      developers: this.store.findAll('developer'),
+      employers: this.store.findAll('employer')
+    });
+  }
 });
