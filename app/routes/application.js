f93073bde942c60e87a0fc2340c84305a0cf66b8
Josue Valverde <josuevalverde12@gmail.com>
2015-10-07 11:54:37 -0700

    add torii authentication routing and files.

---
+++ app/routes/application.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  beforeModel: function() {
+    debugger;
+    return this.get("session").fetch().catch(function() {});
+  },
+
+  actions: {
+    signIn: function(provider) {
+      this.get("session").open("firebase", { provider: provider}).then(function(data) {
+        console.log(data.currentUser);
+      });
+    },
+
+    signOut: function() {
+      this.get("session").close();
+    }
+  }
+});
